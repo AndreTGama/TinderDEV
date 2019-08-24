c8b9fd9288873461e97422e587d106537d6e0ff9
AndreTGama <andre.gama@nexttecnologiadainformacao.com.br>
2019-08-24 14:13:16 -0300

    metade do front

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
-
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -13,7 +12,7 @@ const connectedUsers = {};
 
 io.on('connection', socket => {
   const { user } = socket.handshake.query;
-
+  
   connectedUsers[user] = socket.id;
 });
 
@@ -24,13 +23,12 @@ mongoose.connect('mongodb+srv://TinderDev:TinderDev@cluster0-iwaft.mongodb.net/T
 app.use((req, res, next) => {
     req.io = io;
     req.connectedUsers = connectedUsers;
-  
     return next();
   });
   
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
 
-server.listen(3333);
+  server.listen(3333);
