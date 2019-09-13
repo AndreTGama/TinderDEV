ca4d860ca7b85188da5834ea358ac80ec5deaf67
AndreTGama <andre.gama@nexttecnologiadainformacao.com.br>
2019-09-13 00:04:25 -0300

    ainda nao achei o erro no mais

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 import './Main.css';
 import io from 'socket.io-client';
 import api from '../services/api';
-
+import itsaMatch from '../assets/itsamatch.png';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
-
 export default function Main({match}){
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null)
 
     useEffect(()=> {
         async function loadUsers(){
@@ -28,6 +28,9 @@ export default function Main({match}){
         const socket = io('http://localhost:3333',{
             query: {user: match.params.id }
         });
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
     },[match.params.id]);
 
 
@@ -76,6 +79,16 @@ export default function Main({match}){
                     </ul>
             ):(
                 <div className="empty">Acabou :( </div>
+            )}
+
+            {matchDev &&  (
+                <div className="match-container">
+                    <img src={itsaMatch} alt="It's a match"/>
+                    <img className="avatar" src={matchDev.avatar} alt=""/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button type="button" onClick={() => setMatchDev(null)}>Fechar</button>
+                </div> 
             )}
         </div> 
     );
